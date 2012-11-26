8b8946930f1942a0ea1ac6b1210e69bdcc9584c8
Srinjoy Chakravarty <chaxz93@gmail.com>
2012-11-26 20:48:47 +0300

    rpsec tests with doctor dropdown list all pass

--- spec/views/appointments/new.html.erb_spec.rb
+++ spec/views/appointments/new.html.erb_spec.rb
@@ -15,7 +15,7 @@ describe "appointments/new" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
       assert_select "form", :action => appointments_path, :method => "post" do
       assert_select "input#appointment_patient_id", :name => "appointment[patient_id]"
-      assert_select "input#appointment_doctor_id", :name => "appointment[doctor_id]"
+      assert_select "select#appointment_doctor_id", :name => "appointment[doctor_id]"
       assert_select "select#appointment_time_slot", :name => "appointment[time_slot]"
     end
   end
